00dc1083d38add63a6b04d09cb3b5b1b4f6d181f
mculjak <mateja.culjak@gmail.com>
2017-05-01 00:00:46 +0200

    Cleanup, small changes

--- src/main/scraper/scraper/spiders/arstechnica.py
+++ src/main/scraper/scraper/spiders/arstechnica.py
@@ -6,13 +6,13 @@ class ArstechnicaSpider(BaseSpider):
     name = "arstechnica"
     allowed_domains = ["arstechnica.com"]
     start_urls = (
-        'http://arstechnica.com/gadgets/',
-        'http://arstechnica.com/information-technology/',
-        'http://arstechnica.com/business/',
-        'http://arstechnica.com/tech-policy/',
-        'http://arstechnica.com/gaming/',
-        'http://arstechnica.com/science/',
-        'http://arstechnica.com/security/',
+        'https://arstechnica.com/gadgets/',
+        'https://arstechnica.com/information-technology/',
+        'https://arstechnica.com/business/',
+        'https://arstechnica.com/tech-policy/',
+        'https://arstechnica.com/gaming/',
+        'https://arstechnica.com/science/',
+        'https://arstechnica.com/security/',
     )
 
     def get_url_xpath(self):
--- src/main/scraper/scraper/spiders/techcrunch.py
+++ src/main/scraper/scraper/spiders/techcrunch.py
@@ -5,7 +5,7 @@ from scraper.spiders.base_spider import BaseSpider
 class TechcrunchSpider(BaseSpider):
     name = 'techcrunch'
     allowed_domains = ['techcrunch.com', 'feeds.feedburner.com', 'feedproxy.google.com']
-    start_urls = ['http://feeds.feedburner.com/TechCrunch/']
+    start_urls = ['https://feeds.feedburner.com/TechCrunch/']
 
     def get_url_xpath(self):
         return '//item/link/text()'
